b74b385b0d6fb42e138b81955063f3b8a1c9da60
Aditya Narayan <49934788+dwivediaditya@users.noreply.github.com>
2019-09-06 18:08:40 +0530

    Add files via upload
    database

---
+++ houseofmandidb.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2019 at 09:26 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `houseofmandidb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` varchar(40) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` varchar(40) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `firstName` varchar(40) NOT NULL,
+  `lastName` varchar(40) NOT NULL,
+  `mobileNumber` varchar(10) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `verified` tinyint(1) NOT NULL,
+  `approved` tinyint(1) NOT NULL,
+  `verificationcode` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fooditem`
+--
+
+CREATE TABLE `fooditem` (
+  `id` int(11) NOT NULL,
+  `itemName` varchar(40) NOT NULL,
+  `itemDesc` varchar(200) NOT NULL,
+  `itemPrice` int(11) NOT NULL,
+  `percentOff` int(11) NOT NULL,
+  `actual_price` int(11) NOT NULL,
+  `img_path` varchar(200) NOT NULL,
+  `itemStatus` tinyint(1) NOT NULL,
+  `personCount` int(11) NOT NULL,
+  `categoryID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fooditemcategory`
+--
+
+CREATE TABLE `fooditemcategory` (
+  `id` int(11) NOT NULL,
+  `categoryName` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `itemorder`
+--
+
+CREATE TABLE `itemorder` (
+  `id` int(11) NOT NULL,
+  `orderDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deliveryAddress` varchar(200) NOT NULL,
+  `orderStatus` tinyint(1) NOT NULL,
+  `paymentMode` varchar(40) NOT NULL,
+  `customerID` varchar(40) NOT NULL,
+  `cancelled` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `itemorderdetail`
+--
+
+CREATE TABLE `itemorderdetail` (
+  `id` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `itemOrderID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `fooditem`
+--
+ALTER TABLE `fooditem`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FoodItem_ibfk_1` (`categoryID`);
+
+--
+-- Indexes for table `fooditemcategory`
+--
+ALTER TABLE `fooditemcategory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `itemorder`
+--
+ALTER TABLE `itemorder`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ItemOrder_ibfk_1` (`customerID`);
+
+--
+-- Indexes for table `itemorderdetail`
+--
+ALTER TABLE `itemorderdetail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `itemOrderID` (`itemOrderID`),
+  ADD KEY `itemID` (`itemID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fooditem`
+--
+ALTER TABLE `fooditem`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `fooditemcategory`
+--
+ALTER TABLE `fooditemcategory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `itemorder`
+--
+ALTER TABLE `itemorder`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `itemorderdetail`
+--
+ALTER TABLE `itemorderdetail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `fooditem`
+--
+ALTER TABLE `fooditem`
+  ADD CONSTRAINT `FoodItem_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `fooditemcategory` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `itemorder`
+--
+ALTER TABLE `itemorder`
+  ADD CONSTRAINT `ItemOrder_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `itemorderdetail`
+--
+ALTER TABLE `itemorderdetail`
+  ADD CONSTRAINT `ItemOrderDetail_ibfk_1` FOREIGN KEY (`itemOrderID`) REFERENCES `itemorder` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ItemOrderDetail_ibfk_2` FOREIGN KEY (`itemID`) REFERENCES `fooditem` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
